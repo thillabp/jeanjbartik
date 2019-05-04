3cbbb901803d6dce70e799ad53e0412557e4b77b
thillabp <thillabp@gmail.com>
2019-05-03 22:07:22 -0300

    terceiro commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,11 @@
-document.addEventListener("load", addEventos());
-
 var diva = document.querySelector(".perfil__img-1");
-var autora = document.querySelector("#autora");
 
-function addEventos(){
-   document.querySelector(".perfil__button").addEventListener("click", function(){
-       
-       diva.src="assets/foto_atual1.jpg";
-       
-   });
-    
-   document.querySelector(".perfil__button").addEventListener("dblclick", function(){
-       
-       diva.src="assets/foto_atual.jpg";
-       
-   }); 
-    
-    
-   document.querySelector("#thilla").addEventListener("click", function(){
-       
-       autora.classList.remove("efeito_img");
-       
-   });
 
-   document.querySelector("#thilla").addEventListener("mouseout", function(){
-       
-       autora.classList.add("efeito_img");
-       
-   });
-    
+function mudar_foto1(){
+    diva.src="./assets/foto_atual1.jpg";
 };
+
+function mudar_foto2(){
+    diva.src="./assets/foto_atual.jpg";
+}
 
